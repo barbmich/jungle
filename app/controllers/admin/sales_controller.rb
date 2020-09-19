1a9f48eb6492e0721e1417e1ddb745162dc3bf59
Michele Barbiero <barbmich.since86@gmail.com>
2020-09-19 19:43:53 +0000

    add sales page feature

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,9 +1,20 @@
 class Admin::SalesController < ApplicationController
 
+  before_filter :authenticate
+
   def index
     @sales = Sale.all
   end
 
   def new
   end
+
+  protected
+  
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
+  
 end
